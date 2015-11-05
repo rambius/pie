daec5a404dc584b073c009b6d02b07caf931b74e
rambius <rambiusparkisanius@gmail.com>
2015-11-05 02:47:24 -0500

    added initial Account

---
+++ src/pie/ch09/Account.java
@@ -0,0 +1,31 @@
+package pie.ch09;
+
+public class Account {
+
+    private int userNumber;
+    private String userLastName;
+    private String userFirstName;
+    private double userBalance;
+
+    public boolean deposit(double amount) {
+	double newBalance;
+	if (amount < 0.0) {
+	    return false;
+	} else {
+	    newBalance = userBalance + amount;
+	    userBalance = newBalance;
+	    return true;
+	}
+    }
+
+    public boolean withdraw(double amount) {
+	double newBalance;
+	if (amount < 0.0 || amount > userBalance) {
+	    return false;
+	} else {
+	    newBalance = userBalance - amount;
+	    userBalance = newBalance;
+	    return true;
+	}
+    }
+}
