8c5e85dca65bf5e96c27ac5ccfe18610f5fa7e8d
rambius <rambiusparkisanius@gmail.com>
2015-11-06 01:10:07 -0500

    restricted synchronized blocks

--- src/pie/ch09/Account.java
+++ src/pie/ch09/Account.java
@@ -7,25 +7,29 @@ public class Account {
     private String userFirstName;
     private double userBalance;
 
-    public synchronized boolean deposit(double amount) {
+    public boolean deposit(double amount) {
 	double newBalance;
 	if (amount < 0.0) {
 	    return false;
 	} else {
-	    newBalance = userBalance + amount;
-	    userBalance = newBalance;
+	    synchronized(this) {
+		newBalance = userBalance + amount;
+		userBalance = newBalance;
+	    }
 	    return true;
 	}
     }
 
-    public synchronized boolean withdraw(double amount) {
+    public boolean withdraw(double amount) {
 	double newBalance;
-	if (amount < 0.0 || amount > userBalance) {
-	    return false;
-	} else {
-	    newBalance = userBalance - amount;
-	    userBalance = newBalance;
-	    return true;
+	synchronized(this) {
+	    if (amount < 0.0 || amount > userBalance) {
+		return false;
+	    } else {
+		newBalance = userBalance - amount;
+		userBalance = newBalance;
+		return true;
+	    }
 	}
     }
 }
