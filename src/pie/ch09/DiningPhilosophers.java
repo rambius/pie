f16930e7cdbe0350c2ca04d58495206a55eeba9a
rambius <rambiusparkisanius@gmail.com>
2015-11-08 20:04:59 -0500

    dining philosopher - livelock

---
+++ src/pie/ch09/DiningPhilosophers.java
@@ -0,0 +1,61 @@
+package pie.ch09;
+
+public class DiningPhilosophers {
+
+    private Object[] forks;
+    private Philosopher[] philosophers;
+
+    public DiningPhilosophers(int num) {
+	forks = new Object[num];
+	philosophers = new Philosopher[num];
+	for (int i = 0; i < num; i++) {
+	    forks[i] = new Object();
+	    philosophers[i] = new Philosopher(i, i + 1, (i + 1) % num);
+	}
+    }
+
+    public void startEating() throws InterruptedException {
+	for (int i = 0; i < philosophers.length; i++) {
+	    philosophers[i].start();
+	}
+	philosophers[0].join();
+    }
+
+    private class Philosopher extends Thread {
+	
+	private int id;
+	private int fork1;
+	private int fork2;
+
+	public Philosopher(int id, int fork1, int fork2) {
+	    this.id = id;
+	    this.fork1 = fork1;
+	    this.fork2 = fork2;
+	}
+
+	public void run() {
+	    status("Ready to eat with forks " + fork1 + " and " + fork2);
+	    while (true) {
+		status("Picking up fork " + fork1);
+		synchronized(forks[fork1]) {
+		    status("Picking up fork " + fork2);
+		    synchronized(forks[fork1]) {
+			status("Eating");
+		    }
+		}
+	    }
+	}
+
+	private void status(String msg) {
+	    System.out.println("Philosopher " + id + ": " + msg);
+	}
+    }
+
+    public static void main(String[] args) {
+	try {
+	    DiningPhilosophers d = new DiningPhilosophers(5);
+	    d.startEating();
+	} catch(InterruptedException ie) {
+	}
+    }
+}
